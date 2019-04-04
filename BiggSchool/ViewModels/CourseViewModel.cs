9a4da3d02126755fa414d1af5e01bc49e87a7064
Duc <stt02031998@gmail.com>
2019-04-04 11:14:13 +0700

    thay doi

--- BiggSchool/ViewModels/CourseViewModel.cs
+++ BiggSchool/ViewModels/CourseViewModel.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BiggSchool.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace BiggSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
